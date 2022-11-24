8a1ba33cf105ad2bef5f6705db2a369bce68cc7e
fabien-ricca <fabien.ricca@laplateforme.io>
2022-11-24 16:07:25 +0100

    Jour09 job12

---
+++ jour09/job12/job12.sql
@@ -0,0 +1,4 @@
+/* Trier une table en fonction d'une colonne,
+    la fonction ORDER BY trie par défault de manière croissante (ascendante)
+    en rajoutant le suffixe DESC on peut trier de manière décroissante (déscendante) */
+SELECT * FROM salles ORDER BY capacite DESC;
