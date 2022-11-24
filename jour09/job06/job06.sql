0b9a7a2227a067505a61808a4346f879bcfab79c
fabien-ricca <fabien.ricca@laplateforme.io>
2022-11-24 15:27:50 +0100

    Jour09 job06

--- jour09/job06/job06.sql
+++ jour09/job06/job06.sql
@@ -1,1 +1,5 @@
+/*Méthode avec fonction left : LEFT(chaine, longueur)*/
 SELECT * FROM etudiants WHERE LEFT(prenom, 1) = 'T';
+
+/*Méthode avec like : like 'T'*/
+SELECT * FROM etudiants WHERE prenom like 'T%';
