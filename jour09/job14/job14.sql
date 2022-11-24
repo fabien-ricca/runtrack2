9252ebf81f839cdd8fe391e587f9a8a8ebef2fe0
fabien-ricca <fabien.ricca@laplateforme.io>
2022-11-24 16:18:35 +0100

    Jour09 job14

---
+++ jour09/job14/job14.sql
@@ -0,0 +1,2 @@
+/* Sélectionner des colonnes en vérifiant si la valeur d'une colonne est comprise entre deux autres valeurs */
+SELECT prenom, nom, naissance FROM etudiants WHERE naissance BETWEEN '1998-01-01' AND '2018-01-01';
